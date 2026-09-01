bba7122ee6b7bb393ccb7999e7798a7c78b3140b
Pedro Binatti <pedrobinatti673@gmail.com>
2026-09-01 19:41:02 -0400

    Adicionando WebQuest completa

---
+++ images.pl
@@ -0,0 +1,26 @@
+# LaTeX2HTML 2024 (Released January 1, 2024)
+# Associate images original text with physical files.
+
+
+$key = q//;
+$cached_env_img{$key} = q|<IMG
+ STYLE="height: 31.06ex; vertical-align: -15.01ex; " SRC="|."$dir".q|img17.svg"
+ ALT="\fbox{\begin{minipage}{0.85\textwidth}
+\begin{center}\textcolor{alerta}{\textbf{...
+...uir capturar essa onda antes que outros países dominem o jogo?}
+\end{minipage}}">|; 
+
+$key = q/fbox{minipage{{0.9textwidth}center{textcolor{alerta}{textbf{1.000TWh=1.000.000.000.000kWh}}center{minipage{};MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE="height: 3.10ex; vertical-align: -0.98ex; " SRC="|."$dir".q|img13.svg"
+ ALT="\fbox{\begin{minipage}{0.9\textwidth}
+\begin{center}\textcolor{alerta}{\textbf{1.000 TWh = 1.000.000.000.000 kWh}}
+\end{center}\end{minipage}}">|; 
+
+$key = q/rightarrow;MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE="height: 1.04ex; vertical-align: -0.10ex; " SRC="|."$dir".q|img10.svg"
+ ALT="$\rightarrow$">|; 
+
+1;
+
